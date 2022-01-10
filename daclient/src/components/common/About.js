96b8ee3aea4aea15d7a9389b93f78ad47dd10b03
Joel Marquez <joelhmarquez@gmail.com>
2022-01-09 22:44:22 -0700

    updating about me" clear

--- daclient/src/components/common/About.js
+++ daclient/src/components/common/About.js
@@ -11,12 +11,11 @@ function About()
                     </p>
                     <p>
                         The sections below describe the information that can be found in the two visualization tools, population and performance, respectively.
-                        I will continue to update these tools with additional visualizations of the existing data, as well as visualizations for new data that I haven't gotten a chance to explore as much, such as performance data per birth country and by state of residence, to name a couple.
                     </p>
-                    <p>
-                        USCIS decided to redact some of the data where the number of recipients was under 10 to avoid potentially identify individual recipients.
+                    <b>Please Note:</b>
+                    <p> USCIS decided to redact data where the number of recipients was under 10 to avoid opening up the potential to identify individual recipients.
                         In the reports, they were marked as 'D'; in the visualizations on this site, they are marked as zero.
-                        Because of that, please note that some of the results marked zero may actually be an undisclosed number less than 10.
+                        Because of that, keep in mind that some of the results marked as zero may actually be an undisclosed number less than 10.
                     </p>
                     
                     <h5>Population Data</h5>
@@ -35,25 +34,25 @@ function About()
                         We can also toggle between year for which dataset we wish to use.
                         The second set of visualizations are line graphs in which we can select a country or state from the drop-down lists and examine the population changes throughout the years.
                     </p>
-                    <p>Population data was only included from 2017 onwards and thus we don't have a complete picture, but the information that is available is quite interesting nonetheless.</p>
+                    <p>USCIS began providing population data from 2017 onwards and thus we don't have a complete picture of population changes since the programs inception, however, the information that is available is quite interesting nonetheless.</p>
                     
                     <h5>Performance Data</h5>
-                    <p>The performance data visualization tool explores the administrative data provided by USCIS regarding DACA.</p>
+                    <p>The performance data visualization tool explores the administrative processing data provided by USCIS regarding DACA.</p>
                     <p>
                         In this tool, we can explore the volume of applications received by USCIS throughout the years and how many were accepted and rejected.
-                        We are also able to explore the accepted applications further by examining how many were accepted, rejected and pending for a given year.
+                        We are also able to explore the accepted applications further by examining how many were approved, rejected or remained pending for a given year.
                     </p>
 
                     <h5>About The Creator</h5>
                     <p>
                         My name is Joel Marquez, I am a former DACA recipient originally from Mexico. I have lived in the US since the age of 8 and was a DACA recipient since shortly after the program's creation.
-                        As most DACA recipients will attest to, DACA has opened up a lot of opportunities for those of us lucky enough to qualify.
-                    </p>
+                        As most DACA recipients will attest to, DACA opened up a lot of opportunities for me and allowed me to pursue my ambitions career and life ambitions.
                     <p>
-                        For me, the work authorization and deferred action helped me find a way through college where I graduated in the spring of 2018 with a computer science degree.
+                    </p>
+                        The work authorization and deferred action it provided helped me find a way through college where I graduated in the spring of 2018 with a degree in computer science.
                         It also allowed me to enter the workforce as a professional following my graduation. I joined Microsoft as a software engineer out of college and have been there since.
                         I get to contribute alongside an amazing team of engineers to building the calling and meeting services that power Microsoft Teams and Skype.
-                        It is truly a dream job and I am grateful to work on something that positively improves the lives of the millions of daily users.
+                        It is truly a dream job and I am grateful to work on something that positively improves the lives of the many millions of daily users.
                     </p>
                     <p>
                         Besides playing a big role in my career, Microsoft also played a big role in my immigration story. Shortly after joining the company in 2018, we began an employment-based permanent residency sponsorship process
